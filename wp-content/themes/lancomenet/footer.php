d132a9135dff9c2b986bd5e92896091989dbdaa1
Andrey1901 <andrey.zarudny@gmail.com>
2016-08-13 18:59:36 +0300

    Installation of WP and very beginning of developing: Created theme folder and index, header, footer files

---
+++ wp-content/themes/lancomenet/footer.php
@@ -0,0 +1,25 @@
+<!-- Footer begin here -->
+<section class="container-fluid footer" id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h3>Задайте вопрос</h3>
+                        <form action="#">
+                            <input type="text" placeholder="Ваше имя">
+                            <input type="text" placeholder="Ваш телефон">
+                            <textarea placeholder="Вопрос"></textarea>
+                            <a href="#" type="submit" class="send">отправить</a>
+                        </form>
+                    </div>
+                    <div class="col-sm-6 contacts clearfix">
+                        <img src="img/logo-footer.png" class="hidden-xs hidden-sm" alt="logo-footer">
+                        <span>+38 (057) 720-44-77</span>
+                        <span>+38 (097) 376-44-77</span>
+                        <span>+38 (063) 167-44-77</span>
+                        <i>© 2016 «LanComNet» Все права защищены. Design Aleksey Sendetskiy</i>
+                    </div>
+                </div>
+            </div>
+        </section>
+<!-- Footer end here-->
+    </body>
---
+++ wp-content/themes/lancomenet/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8" />
+    <title>LanComNet</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- <link type="text/css" href="css/main.css" rel="stylesheet" media="screen" />
+    <link type="text/css" href="css/media.css" rel="stylesheet" media="screen" /> -->
+    <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/9636700c7b.js"></script>
+    <script type="text/javascript" src="js/common.js" ></script>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+<!-- Header -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="row">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.png" class="logo-white" alt="logo">
+                </a>
+            </div>
+            <div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav pull-right">
+                        <li class="active"><a href="index.html" class="link">ГЛАВНАЯ</a></li>
+                        <li class="drop">
+                            <a href="javascript:void(0);" class="link">услуги</a>
+                            <div class="dropdown-holder">
+                                <ul>
+                                    <li><a href="build-web.html">Строительство сетей</a></li>
+                                    <li><a href="video.html">Видеонаблюдение</a></li>
+                                    <li><a href="skud.html">СКУД Контроль доступа</a></li>
+                                    <li><a href="pon.html">pon</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="works.html" class="link">наши работы</a></li>
+                        <li><a href="price.html" class="link">Прайс</a></li>
+                        <li><a href="contacts.html" class="link">контакты</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- End Header -->
